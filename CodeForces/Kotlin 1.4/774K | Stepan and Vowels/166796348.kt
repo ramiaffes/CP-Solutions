a9cef5954491bcbb5b3c3fc0b6a9c063c58d24da
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:18:21 +0100

    Committing files

---
+++ CodeForces/Kotlin 1.4/774K | Stepan and Vowels/166796348.kt
@@ -0,0 +1,40 @@
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readLongs() = readStrings().map { it.toLong() }// list of ints
+private fun readInts() = readStrings().map { it.toInt() }// list of ints
+fun main(args: Array<String>) {
+var n=readInt()
+var str=readLn()
+var test=false
+var test2=true
+val res = mutableListOf<Char>(str[0])
+var str3="aiuy"
+for(i in 1..(n-1)){
+if((str[i]==str[i-1])and(str3.contains(str[i]))){
+continue}
+else if ((str[i]==str[i-1])and((str[i]=='e')or(str[i]=='o'))){
+if(test2==false)continue
+if(test==true){
+test2=false 
+continue
+}
+test=true 
+if(i==(n-1)){
+res.add(str[i])}
+continue
+}
+else if(str[i]!=str[i-1]){
+if((str[i-1]=='e')or(str[i-1]=='o')){
+if((test==true)and(test2==true)){
+res.add(str[i-1])
+}}
+test2=true
+test=false}
+res.add(str[i])
+ 
+}
+println(res.joinToString(""))
+ 
+}
