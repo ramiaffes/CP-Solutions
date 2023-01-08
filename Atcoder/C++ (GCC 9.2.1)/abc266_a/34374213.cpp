24e18cc7d1447c1591e10cffa73bdc6f34759561
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:14:38 +0100

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc266_a/34374213.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; 
+map<long long int,long long int>adj;
+map<long long int,bool>vis;
+map<long long int,bool>res1;
+map<long long int,bool>res2;vector<long long int>vect(N);
+ 
+int main()
+{
+	fast;	
+	string s;
+	cin>>s;
+	cout<<(s[((s.length()+1)/2)-1])<<endl;
+	
+	
+	
+    
+    
+return 0;}
+ 
