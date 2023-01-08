f3350124e814e93d60c76459e786d9556911b1a1
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:20:22 +0100

    Committing files

---
+++ CodeForces/GNU C++17/49C | Disposition/161383564.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; map<long long int ,vector<long long int>>adj;
+long long int val=0;
+map<long long int,bool>vis;
+map<long long int,bool>vis1;
+map<long long int,long long int>parent;
+long long int som=0;
+map<long long int,long long int>var;
+map<long long int,long long  int>hashing;
+bool test=false;
+int main()
+{
+ fast; 
+ long long int n,x,y;
+ cin>>n;
+ for(long long int i(1);i<n;i++){
+   cout<<(i+1)<<" ";
+  }
+ cout<<1<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+    
+    
+return 0;}
