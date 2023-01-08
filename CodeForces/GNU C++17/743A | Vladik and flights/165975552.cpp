6d6d21deccfbb34106894f6e282f98afb8268f4e
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:18:52 +0100

    Committing files

---
+++ CodeForces/GNU C++17/743A | Vladik and flights/165975552.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; 
+map<long long int,long long int>adj;
+long long int ans=-1e5;
+ 
+long long int som=0;
+int main()
+{
+ fast; 
+ long long int n,a,b;
+ string s;
+ cin>>n>>a>>b;
+ cin>>s;
+ if(s[b-1]==s[a-1]){
+  cout<<0<<endl;}
+ else{
+  cout<<1<<endl;
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+    
+    
+return 0;}
