de168860575b9212bd0469439d1aa2dfae9593a6
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:15:55 +0100

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1345B | Card Constructions/176678283.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4;
+map<long long int,long long int>dp;
+map<long long int,bool>vis;
+int main()
+{
+ fast; 
+ long long int n,t;
+ cin>>t;
+ while(t--){
+  cin>>n;
+  vector<long long int>dp(5*1e4);
+  dp[0]=2;
+  long long int som10=5;
+  long long int re=0;
+  for(long long int i(1);i<5*1e4;i++){
+   dp[i]=dp[i-1]+som10;
+   if(dp[i]>n){re=i+1;break;}
+   som10+=3;}
+  dp.resize(re);
+   long long int som=0;
+   while(n>=0){
+   auto it=upper_bound(dp.begin(),dp.end(),n);
+   if(it==dp.begin())break;
+   som++;
+   n-=*(it-1);}
+  cout<<som<<endl;
+ }
+ 
+ 
+   
+ 
+  
+  
+  
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+    
+return 0;} 
