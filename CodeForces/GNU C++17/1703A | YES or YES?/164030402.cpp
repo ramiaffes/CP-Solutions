09ea663923efb1d85cc2b4d2beabcaa21f84033b
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:19:41 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1703A | YES or YES?/164030402.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; map<pair<long long int,long long int>,vector<pair<long long int,long long int>>>adj;
+int main()
+{
+ fast; 
+ long long int t,n,k,a,b;
+ 
+ cin>>t;
+ while(t--){ string s2;
+  cin>>s2;
+  if((toupper(s2[0])=='Y')and((toupper(s2[1])=='E'))and((toupper(s2[2])=='S')))cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  
+  
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+    
+    
+return 0;}
