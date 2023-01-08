efc0ee1fb90e9c22f02d7097bffece5cf87db15b
ramiaffes <76079346+ramiaffes@users.noreply.github.com>
2023-01-08 05:17:38 +0100

    Committing files

---
+++ CodeForces/Kotlin 1.4/1698A | XOR Mixup/167646501.kt
@@ -0,0 +1,24 @@
+import kotlin.math.abs
+import kotlin.math.min
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readLongs() = readStrings().map { it.toLong() }// list of ints
+private fun readInts() = readStrings().map { it.toInt() }// list of ints
+ 
+fun main(args: Array<String>) {
+var t=readInt()
+for( i in 0..(t-1)){
+var n=readInt()
+var vect=readInts()
+println(vect[0])}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
