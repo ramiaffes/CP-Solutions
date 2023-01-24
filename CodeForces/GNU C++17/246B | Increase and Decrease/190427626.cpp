37ed603137626081a25141286dc9491995c54556
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-24 21:51:36 +0100

    Committing files

---
+++ CodeForces/GNU C++17/246B | Increase and Decrease/190427626.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast;
+ long long int n;
+ cin>>n;
+ vector<long long int>vect(n);
+ long long int som=0;
+ for(long long int i(0);i<n;i++){cin>>vect[i];som+=vect[i];}
+ if(som%n==0){
+  cout<<n<<endl;}
+ else{
+  cout<<(n-1)<<endl;}
+ 
+ 
+ 
+ 
+ 
+ 
+   
+   
+    
+    
+    
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+ 
+ 
+ 
+ 
+ 
+return 0;}
