930dc3e7e272a4ac175e188b21eba30b9cbf148a
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-24 21:21:39 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1722A | Spell Check/170174383.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; map<pair<long long int,long long int>,vector<pair<long long int,long long int>>>adj;
+long long int power(long long int n,long long int k){
+ long long int p=1;
+ for(long long int i(0);i<k;i++){
+  p*=n;}
+ return p;}
+int main()
+{
+ fast; 
+ long long int n,t;
+ cin>>t;
+ string s;
+ while(t--){
+  cin>>n;
+  cin>>s;
+  string s1="Timur";
+  sort(s1.begin(),s1.end());
+  sort(s.begin(),s.end());
+  if(s==s1)cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;}
+  
+ 
+ 
+ 
+ 
+ 
+    
+    
+return 0;}
