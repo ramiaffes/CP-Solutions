d4847b96249726e976ce106b6dab1389f47f2b5b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-24 21:21:51 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1711A | Perfect Permutation/165540245.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4; 
+map<long long int,long long int>adj;
+long long int ans=-1e5;
+ 
+long long int som=0;
+int main()
+{
+ fast; 
+ long long int n,t,s,x,y;
+ cin>>t;
+ while(t--){
+  cin>>n;
+  for(long long int i(1);i<n;i++){
+   cout<<(i+1)<<" ";
+   }
+  cout<<1<<endl;
+ }
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+    
+    
+return 0;}
