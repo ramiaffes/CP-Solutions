862bfb58c8223f266cb535957bae280940bbd478
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-24 21:49:46 +0100

    Committing files

---
+++ CodeForces/GNU C++17/312B | Archer/190428008.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e4;
+map<char,set<char>>adj;
+map<char,bool>vis;
+long long int som=0;
+map<pair<char,char>,long long int>val;
+map<char,long long int>dist1;
+map<char,long long int>dist2;
+ 
+int main()
+{
+ fast; 
+  double a,b,c,d;
+ string s1,s2;
+ cin>>a>>b>>c>>d;
+  double var=(1-a/b)*(1-c/d);
+ printf("%.12f",((1/(1-var))*(a/b)));
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+   
+  
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
