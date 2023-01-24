05d3667c6616615f42898c43053dbe655be3bab9
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-24 21:18:31 +0100

    Committing files

---
+++ CodeForces/GNU C++17/922A | Cloning Toys/190419304.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+const  long long int N=1e5+5;
+map<pair<long long int ,long long int>,long long int>dist;
+int main()
+{
+ fast;
+ long long int x,y;
+ string a,b;
+ cin>>x>>y;
+ if((y==1)and(x==0)) cout<<"YES"<<endl;
+ else if(((y>=2)and(x>=(y-1))and((x-y+1)%2==0))){
+  cout<<"YES"<<endl;}
+ else{
+  cout<<"NO"<<endl;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+ 
+ 
+ 
+ 
+ 
+return 0;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
