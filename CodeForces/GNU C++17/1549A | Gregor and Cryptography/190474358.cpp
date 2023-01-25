89c73c1ac76e9b2b09fa426152ab094e1e8d26ba
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:37:07 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1549A | Gregor and Cryptography/190474358.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  t,p;
+ cin>>t;
+ while(t--){
+  cin>>p;
+  cout<<(2)<<" "<<(p-1)<<endl;
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
