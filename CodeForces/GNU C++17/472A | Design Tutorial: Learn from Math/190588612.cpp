ab568907f01dbe178df05fb0896e118903f9abae
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:49:43 +0100

    Committing files

---
+++ CodeForces/GNU C++17/472A | Design Tutorial: Learn from Math/190588612.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+ 
+ 
+int main()
+{
+ fast;
+ int n;
+ cin>>n;
+ if (n%2==0){
+  cout<<4+n-12<<" "<<8;}  
+ else {
+  cout<<4+n-13<<" "<<9;}
+ 
+ 
+ return 0;
+}
