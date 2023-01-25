c8bec8f2547c63739dfdc5506e1cdd5a56a58c9e
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:08:05 +0100

    Committing files

---
+++ CodeForces/GNU C++17/630C | Lucky Numbers/190479467.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+long long int power(long long int g,long long int k){
+ long long int p=1;
+ for(long long int i(0);i<k;i++){
+  p*=g;}
+ return p;}
+int main()
+{
+ fast; 
+ long long int t,n;
+ cin>>n;
+ cout<<(power(2,(n+1))-2)<<endl;
+ 
+  
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
