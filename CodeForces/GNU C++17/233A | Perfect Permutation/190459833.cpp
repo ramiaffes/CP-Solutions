365c6ba52191c92b823a793e8e92ea2d8ecd0073
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 09:09:54 +0100

    Committing files

---
+++ CodeForces/GNU C++17/233A | Perfect Permutation/190459833.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long  int n;
+ cin>>n;
+ if(n%2==1)cout<<"-1"<<endl;
+ else{
+ vector<long long int>vect(n);
+ for(long long int i(0);i<n;i++)
+ {
+  if(i%2==0)cout<<(i+2)<<" ";
+  else cout<<(i)<<" ";}}
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+  
+  
+  
+  
+ 
+  
+  
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
