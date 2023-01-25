81e309bad367dd13c50a1fb78193b59b64745c9f
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 09:09:26 +0100

    Committing files

---
+++ CodeForces/GNU C++17/630K | Indivisibility/190460091.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long  int n;
+ cin>>n;
+ cout<<(n-n/3-n/2-n/5-n/7+n/14+n/10+n/6+n/21+n/15+n/35-n/30-n/42-n/70-n/105+n/210)<<endl;
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+  
+  
+  
+  
+ 
+  
+  
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
