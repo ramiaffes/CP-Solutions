9883aee9b9875d0ee3d1975ca785eff68f50a0b7
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:53:51 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1269A | Equation/190484323.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int n,t;
+ cin>>n;
+ cout<<(9*n)<<" "<<(8*n)<<endl;
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
