e00b66e9198704cef4d7e2d992c840b86ff7778f
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:36:12 +0100

    Committing files

---
+++ CodeForces/GNU C++17/912A | Tricky Alchemy/190585455.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ long long int A,B,x,y,z;
+ cin>>A>>B;
+ cin>>x>>y>>z;
+ long long int var(0);
+ cout<<(max(2*x+y-A,var)+max(3*z+y-B,var))<<endl;
+ 
+ 
+ 
+ 
+  
+  
+   
+  
+  
+  
+ 
+ 
+  
+ 
+ 
+ 
+    
+    
+ 
+    
+   
+    
+    
+    return 0;
+}
