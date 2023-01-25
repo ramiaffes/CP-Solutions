f3357fb8d7a9c76b4c2a96facee26aab763e8043
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:36:20 +0100

    Committing files

---
+++ CodeForces/GNU C++17/347A | Difference Row/190476113.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int n;
+ cin>>n;
+ vector<long long int>vect(n);
+ for(long long int i(0);i<n;i++){
+  cin>>vect[i];
+  }
+ sort(vect.begin(),vect.end());
+ swap(vect[0],vect[n-1]);
+ for(long long int i(0);i<n;i++){
+  cout<<vect[i]<<" ";}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
