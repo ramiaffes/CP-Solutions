73a316d566a0fa4bd4ca490518e5dd4ab677f0b3
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:55:34 +0100

    Committing files

---
+++ CodeForces/GNU C++17/755A | PolandBall and Hypothesis/190483253.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  n;
+ cin>>n;
+ if(n==999)cout<<1<<endl;
+ else if(n==1000)cout<<1<<endl;
+ else if(n==1001)cout<<1<<endl;
+ else cout<<(n+2)<<endl;
+  
+   
+  
+    
+  
+  
+  
+  
+ 
+ 
+   
+  
+  
+  
+  
+  
+  
+ 
+ 
+   
+  
+  
+  
+  
+  
+  
+     
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+ 
+  
+ 
+  
+  
+   
+  
+  
+  
+  
+ 
+   
+  
+  
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+  
+ 
+ 
+ 
+ return 0;
+}
