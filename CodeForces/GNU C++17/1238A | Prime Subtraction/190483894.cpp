7eb5e6b4bac4c200e2677a8ce6423053be9b9402
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:54:27 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1238A | Prime Subtraction/190483894.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+ 
+int main()
+{
+ fast; 
+ long long int t,x,y;
+ cin>>t;
+ while(t--){
+  cin>>x>>y;
+  if((x-y)==1){
+   cout<<"NO"<<endl;}
+  else{
+   cout<<"YES"<<endl;}
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
