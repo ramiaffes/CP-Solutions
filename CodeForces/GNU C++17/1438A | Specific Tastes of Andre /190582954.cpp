1b6904837232491110c5362dae8f05298a25615b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 19:54:16 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1438A | Specific Tastes of Andre /190582954.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+bool sortbysecasc(const pair<int,int> &a,
+                   const pair<int,int> &b)
+{
+       return a.second<b.second;
+}  
+bool is(long long int i){
+ return (i%2==0);}
+int main() {
+ fast;
+    long long int t,n;
+    cin>>t;
+    while(t--){
+  cin>>n;
+  for(long long int i(0);i<n;i++){
+   cout<<1<<" ";}
+   cout<<endl;
+  
+  
+}
+    
+    
+    return 0;
+}
