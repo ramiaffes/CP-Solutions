9702a38dc1f605331c1b2b40d877a1283aea732e
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:08:22 +0100

    Committing files

---
+++ CodeForces/GNU C++17/630A | Again Twenty Five!/190479144.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+long long int power(long long int g,long long int k){
+ long long int p=1;
+ for(long long int i(0);i<k;i++){
+  p*=g;}
+ return p;}
+int main()
+{
+ fast; 
+ long long int n;
+ cin>>n;
+ cout<<25<<endl;
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
