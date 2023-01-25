cad58ba57b3487dd39670d9629c764a66980912c
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:55:09 +0100

    Committing files

---
+++ CodeForces/GNU C++17/664A | Complicated GCD/190483515.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ string  a,b;
+ cin>>a>>b;
+ if(a==b)cout<<a<<endl;
+ else cout<<1<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
