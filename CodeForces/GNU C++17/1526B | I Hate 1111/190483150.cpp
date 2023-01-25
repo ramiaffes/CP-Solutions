b1de815a3507d4888bb5511a1dc0c6ce04dcfd7c
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:55:51 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1526B | I Hate 1111/190483150.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int  n;
+ long long int t;
+cin>>t;
+while(t--){
+  cin>>n;
+  if(n/11>=((n%11)*10)){
+   cout<<"YES"<<endl;}
+  else{
+   cout<<"NO"<<endl;}
+  
+      
+  
+  
+   
+  
+    
+  
+  
+  
+  
+ 
+ 
+   }
+  
+  
+  
+  
+  
+  
+ 
+ 
+   
+  
+  
+  
+  
+  
+  
+     
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+ 
+  
+ 
+  
+  
+   
+  
+  
+  
+  
+ 
+   
+  
+  
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+  
+ 
+ 
+ 
+ return 0;
+}
