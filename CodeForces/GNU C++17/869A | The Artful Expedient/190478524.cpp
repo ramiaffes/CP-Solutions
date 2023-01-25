bed00a52230d899f1812be36c779f02267cb9098
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:08:57 +0100

    Committing files

---
+++ CodeForces/GNU C++17/869A | The Artful Expedient/190478524.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int n,t;
+  cin>>n;
+  vector<long long int>vect(n);
+  vector<long long int>vect1(n);
+  for(long long int i(0);i<n;i++){
+   cin>>vect[i];}
+  for(long long int i(0);i<n;i++){
+   cin>>vect1[i];}
+  cout<<"Karen"<<endl;
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
