064abcec296513cb27bf941e2373d18e6a2aa07b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:35:44 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1081A | Definite Game/190586061.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ long long int v;
+ cin>>v;
+ if(v==1){cout<<1<<endl;}
+ else if (v==2){
+  cout<<2<<endl;}
+ else{cout<<1<<endl;}
+   
+    
+    
+ 
+    
+   
+    
+    
+    return 0;
+}
