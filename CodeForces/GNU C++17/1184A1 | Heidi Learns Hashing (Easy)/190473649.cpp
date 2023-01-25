3f2af335de2c0682b2166aef2efe117085a35e39
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:37:46 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1184A1 | Heidi Learns Hashing (Easy)/190473649.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  r;
+ cin>>r;
+ if(r==1){
+  cout<<"NO"<<endl;}
+ else if(r==3){
+  cout<<"NO"<<endl;}
+ else if(r%2==1){
+  cout<<1<<" "<<((r-3)/2)<<endl;}
+ else{
+  cout<<"NO"<<endl;
+  }
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
