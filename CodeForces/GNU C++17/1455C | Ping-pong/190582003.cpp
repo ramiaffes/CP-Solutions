854dc18c19557852282f4d792ca126efa78b01da
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 19:55:22 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1455C | Ping-pong/190582003.cpp
@@ -0,0 +1,15 @@
+ 
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ 
+  long long int t,x,y;
+  cin>>t;
+  while(t--){ 
+   cin>>x>>y;
+   cout<<(x-1)<<" "<<y<<endl;}
+  
+  
+  return 0;}
