d6a1697f89c280ae887329167a5ec9760aaf6f4f
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:35:53 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1421A | XORwice/190585753.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ long long int t;
+ long long int a,b;
+ cin>>t;
+ while(t--){
+  cin>>a>>b;
+ cout<<(a^=b)<<endl;
+ 
+}
+  
+ 
+ 
+ 
+    
+    
+ 
+    
+   
+    
+    
+    return 0;
+}
