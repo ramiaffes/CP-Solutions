52521e151db7a6e95e25cc80257d6ddeb014eba9
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 09:06:28 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1574A | Regular Bracket Sequences/190461093.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int n,t,k;
+ string s;
+ cin>>t;
+ while(t--){
+  cin>>n;
+ for(long long int k(0);k<n;k++){
+  for(long long int i(0);i<k;i++){
+   cout<<'(';}
+  for(long long  int i(0);i<(k);i++){
+   cout<<')';}
+  for(long long int i(0);i<(n-k);i++){
+   cout<<'(';}
+  for(long long int i(0);i<(n-k);i++){
+   cout<<')';}
+  cout<<endl;}
+  
+ 
+  
+  
+   
+  }
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
