1463a23724f9f56aa2250b7b324bcaa65c8220e6
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 19:55:11 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1454A | Special Permutation/190582143.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+bool iscarre(long long int n){
+ long long int var=sqrt(n);
+ return((var*var)==n);}
+int main() {
+ fast;
+ 
+  long long int t,n;
+  cin>>t;
+  while(t--){
+   cin>>n;
+   for(long long int i(1);i<n;i++){
+    cout<<(i+1)<<" ";}
+    cout<<1<<" ";
+    cout<<endl;
+   
+    
+   
+   
+   
+  }
+  
+   
+  
+  
+    return 0;
+}
