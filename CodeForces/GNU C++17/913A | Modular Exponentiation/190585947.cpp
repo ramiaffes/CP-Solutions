56d8d17713d24b23d5b215921c31921826f8bf3e
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:35:48 +0100

    Committing files

---
+++ CodeForces/GNU C++17/913A | Modular Exponentiation/190585947.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ long long int n,m;
+ cin>>n>>m;
+ long long int c=powl(2,n);
+ cout<<(m%c)<<endl;
+ 
+    
+    
+ 
+    
+   
+    
+    
+    return 0;
+}
