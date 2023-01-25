f8887f19c14ff10529afbf640032e84017ac7c90
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 07:49:40 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1519B | The Cake Is a Lie/190454108.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int n,m,t,k;
+ cin>>t;
+ while(t--){
+ cin>>n>>m>>k;
+ if(n*m-1==k)cout<<"YES"<<endl;
+ else cout<<"NO"<<endl;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+  
+  
+  
+  
+ 
+  
+  
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
