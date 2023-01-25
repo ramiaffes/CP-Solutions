3cba776025742ee01b895a5b2870231a840f3da5
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:08:48 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1038B | Non-Coprime Partition/190478726.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int n;
+ cin>>n;
+ if((n==1)or(n==2)){
+  cout<<"No"<<endl;
+  }
+ else{
+  cout<<"Yes"<<endl;
+   cout<<(n-1)<<" ";
+   for(long long int i(1);i<=(n-1);i++){
+    cout<<i<<" ";}
+   cout<<endl;
+   cout<<(1)<<" "<<n<<endl;
+   
+  }
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
