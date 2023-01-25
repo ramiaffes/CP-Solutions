47f916005d6ccb5b83012260d9f588bb85ee137c
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:37:28 +0100

    Committing files

---
+++ CodeForces/GNU C++17/630J | Divisibility/190474047.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  n;
+ cin>>n;
+ cout<<(n/2520)<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
