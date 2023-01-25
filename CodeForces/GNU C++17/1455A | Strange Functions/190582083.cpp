927154e59d924a56f3177ab67a490eb824a06f0b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 19:55:16 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1455A | Strange Functions/190582083.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+int main() {
+ fast;
+ 
+  long long int t;
+  string s,n;
+  cin>>t;
+  long long int som;
+  while(t--){ 
+   cin>>n;
+   cout<<n.length()<<endl;
+   
+   
+   }
+  
+  return 0;}
