a3e3499bd7cd0a0fbc6422ee2956f58fc859e606
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:37:00 +0100

    Committing files

---
+++ CodeForces/GNU C++17/742A | Arpa’s hard exam and Mehrdad’s naive cheat/190474433.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  n;
+ cin>>n;
+ if(n==0)cout<<1<<endl;
+ else{
+  if(n%4==1){
+   cout<<8<<endl;}
+  else if(n%4==0){
+   cout<<6<<endl;}
+  else if(n%4==2){
+   cout<<4<<endl;}
+  else{
+   cout<<2<<endl;}}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
