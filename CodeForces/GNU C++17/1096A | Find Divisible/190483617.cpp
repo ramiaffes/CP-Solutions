afcf21ebc6149c108532fbb4cff8af9708438a8b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:54:52 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1096A | Find Divisible/190483617.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int l,r,t;
+ 
+ cin>>t;
+ while(t--){
+ cin>>l>>r;
+ cout<<l<<" "<<2*l<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+ 
+return 0;}
