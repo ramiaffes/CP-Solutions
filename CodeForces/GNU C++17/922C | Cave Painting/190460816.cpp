a41bef5fa10f405b9c5eeb8c1e3f945f4e263e58
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 09:07:17 +0100

    Committing files

---
+++ CodeForces/GNU C++17/922C | Cave Painting/190460816.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int main()
+{
+ fast; 
+ long long int n,k;
+ cin>>n>>k;
+ bool test=true;
+ for(long long int i(1);i<=k;i++){
+  if((n%i)!=(i-1)){test=false;break;}}
+ if(test==false)cout<<"NO"<<endl;
+ else cout<<"YES"<<endl;
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
