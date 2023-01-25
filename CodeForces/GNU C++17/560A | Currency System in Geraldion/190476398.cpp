550333e7ac93fbdde3c6975414d1683e521d6907
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:36:01 +0100

    Committing files

---
+++ CodeForces/GNU C++17/560A | Currency System in Geraldion/190476398.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int n;
+ cin>>n;
+ vector<long long int>vect(n);
+ for(long long int i(0);i<n;i++){
+  cin>>vect[i];
+  }
+ sort(vect.begin(),vect.end());
+ if(vect[0]==1)cout<<-1<<endl;
+ else cout<<1<<endl;
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
