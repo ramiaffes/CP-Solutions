fc422d765e97840a0c9ac21758786a460229c729
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:54:30 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1550A | Find The Array/190483873.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+ 
+int main()
+{
+ fast; 
+ long long int t,s;
+ cin>>t;
+ while(t--){
+  cin>>s;
+ long long int n=0;
+ while((n*(n+2)+1)<s){
+  n++;
+  }
+ cout<<(n+1)<<endl;
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
