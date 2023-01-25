d2ffd055f2a7b7c784d22fce08171a7fc53fcc79
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 20:35:09 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1369A | FashionabLee/190586586.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+typedef  vector <long long int> vecteur ;
+ int num(char cr){
+ return (int)cr-48;} 
+int main()
+{
+ fast;
+ long long int t;
+ long long int n;
+ cin>>t;
+ while(t--){
+  cin>>n;
+  if(n%4==0){
+   cout<<"YES"<<endl;}
+  else{
+   cout<<"NO"<<endl;}
+  }
+ 
+ 
+  
+  
+ 
+        
+ return 0;
+}
