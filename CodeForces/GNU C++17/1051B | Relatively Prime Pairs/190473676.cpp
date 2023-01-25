09e1b33ef93c9c0b1a7d8968afc7dcfd5e82c7a9
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 12:37:42 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1051B | Relatively Prime Pairs/190473676.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int  l,r;
+ cin>>l>>r;
+ cout<<"YES"<<endl;
+ for(long long int i(l);i<=(r-1);i+=2){
+  cout<<(i)<<" "<<(i+1)<<endl;}
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
