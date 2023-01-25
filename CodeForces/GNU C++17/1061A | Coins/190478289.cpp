9c254e8f56385c055d154631e4cf5d349478850b
rami affes <76079346+ramiaffes@users.noreply.github.com>
2023-01-25 13:09:15 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1061A | Coins/190478289.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ 
+int main()
+{
+ fast; 
+ long long int n;
+ long long int s;
+ cin>>n>>s;
+ cout<<((s/n)+(s%n!=0))<<endl;
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+ 
+ 
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
